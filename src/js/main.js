b7d45fb834d4332676d489649f468997da7e998a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-02 09:05:04 -0700

    coded the full website

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$('.port-item').click(function() {
+	$('.collapse').collapse('hide')
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
+
+
